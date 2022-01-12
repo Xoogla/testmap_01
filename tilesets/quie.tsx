ba98191fcb5a66ec9089445718ddd81f46248761
Xoogla <95443474+Xoogla@users.noreply.github.com>
2022-01-12 14:25:37 +0100

    del quiet

--- tilesets/quie.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="quie" tilewidth="32" tileheight="32" tilecount="320" columns="16">
- <image source="../../../../AppData/Local/Temp/2/quiet-g0ac617734_640.png" width="531" height="640"/>
-</tileset>
