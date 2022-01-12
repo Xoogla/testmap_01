bf84f220829588be64309d67344f2905ed6d3811
Xoogla <95443474+Xoogla@users.noreply.github.com>
2022-01-12 15:11:40 +0100

    Delete silent.tsx

--- tilesets/silent.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="silent" tilewidth="32" tileheight="32" tilecount="30" columns="5">
- <image source="quiet.png" width="160" height="193"/>
-</tileset>
